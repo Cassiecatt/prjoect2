d4e9f8386895e01ee03a1d3108a7315cb93d6d1c
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-07 18:26:47 -0700

    added put and delete routes for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   User.findOne({
     where: {
-      id: req.param.id,
+      id: req.params.id,
     },
   })
     .then((dbUserData) => {
@@ -35,19 +35,53 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
   })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // PUT /api/users/1
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserdata) => {
+      if (!dbUserdata) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserdata);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // DELETE /api/users/1
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
