03747a0cab39c514f7fc4f8b7708624362b6e385
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-13 13:26:47 -0700

    removed file

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,8 @@
 function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("username-signup").value.trim();
-  const password = document.querySelector("password-signup").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
   if (username && password) {
     fetch("/api/users", {
