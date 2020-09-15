b5a017adefcf6834fc22e5461d91e9dfd72c1a79
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-14 23:11:02 -0700

    styled listing column

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -7,28 +7,26 @@ const { Post, User, Category } = require("../models");
 // });
 
 router.get("/", (req, res) => {
-    Post.findAll({
-      where: {
-        user_id: req.session.user_id
-      },
-      attributes: [
-          "title",
-          "description",
-          "salary",
-      ],
-    })
-      .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        console.log(posts)
-        res.render("dashboard", {
-            laout: 'main',
-          posts, loggedIn: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["title", "description", "salary"],
+    include: [{ model: Category, attributes: ["category_name"] }],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      console.log(posts);
+      res.render("dashboard", {
+        laout: "main",
+        posts,
+        loggedIn: true,
       });
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
