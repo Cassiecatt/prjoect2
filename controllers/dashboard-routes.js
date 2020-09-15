65023f8d389dba58cdfb29395ee4b56f45ccfba9
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-14 18:29:26 -0700

    added job list section to dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,32 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Category} = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Category } = require("../models");
 
-router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
+router.get("/", (req, res) => {
+  res.render("dashboard", { loggedIn: true });
 });
 
-
-
+router.get("/", (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "description", "salary", "category_id"],
+    include: [
+        {
+            model: Category,
+            attributes: ['id', 'category_name']
+        }
+    ]
+  })
+  then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+  })
+  .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
 
 module.exports = router;
