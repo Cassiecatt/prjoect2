23914f7710ec2e3377dae7088323b5f5c94f2f16
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-09-14 20:14:55 -0700

    added else if statement

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -6,27 +6,14 @@ router.get("/", (req, res) => {
   res.render("dashboard", { loggedIn: true });
 });
 
-router.get("/", (req, res) => {
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-    attributes: ["id", "title", "description", "salary", "category_id"],
-    include: [
-        {
-            model: Category,
-            attributes: ['id', 'category_name']
-        }
-    ]
-  })
-  then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
-  })
-  .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-  });
-});
+// router.get('/', (req, res) => {
+//     Post.findAll(
+//         {
+//             where: {
+//                 user_id: req.params.id
+//             }
+//         }
+//     )
+// })
 
 module.exports = router;
